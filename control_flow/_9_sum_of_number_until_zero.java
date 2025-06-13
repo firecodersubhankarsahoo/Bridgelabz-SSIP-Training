6c1856ac3da113aa33c394ecd2a2b410aa098152
subhankar <sahoosubhankar98325@gmail.com>
2025-06-13 10:39:09 +0530

    [subhankar] add. sum the number until press zero

---
+++ control_flow/_9_sum_of_number_until_zero.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class _9_sum_of_number_until_zero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int n=sc.nextInt();
+            if(n==0){
+                break;
+            }else{
+                System.out.println(n);
+            }
+        }
+
+    }
+}
