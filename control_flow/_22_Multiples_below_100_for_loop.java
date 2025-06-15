abccc03130955790083af32cc2b8fdc32a89815c
subhankar <sahoosubhankar98325@gmail.com>
2025-06-15 22:46:28 +0530

    [subhankar] add . _22_Multiples_below_100_for_loop added

---
+++ control_flow/_22_Multiples_below_100_for_loop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class _22_Multiples_below_100_for_loop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        for (int i = 100; i >= 1; i--) {
+            if (i % number == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
