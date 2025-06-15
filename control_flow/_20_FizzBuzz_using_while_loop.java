541cebaee8f06a39c3700dc4ea8115b24724f9d9
subhankar <sahoosubhankar98325@gmail.com>
2025-06-15 22:27:29 +0530

    [subhankar] add . _20_FizzBuzz_using_while_loop added

---
+++ control_flow/_20_FizzBuzz_using_while_loop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class _20_FizzBuzz_using_while_loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int i = 1;
+        while (i <= num) {
+            if (i % 3 == 0 && i % 5 == 0) System.out.println("FizzBuzz");
+            else if (i % 3 == 0) System.out.println("Fizz");
+            else if (i % 5 == 0) System.out.println("Buzz");
+            else System.out.println(i);
+            i++;
+        }
+
+    }
+}
