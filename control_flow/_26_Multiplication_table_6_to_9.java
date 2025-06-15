8e49e505cd70c97942294785dbe273a8ad685566
subhankar <sahoosubhankar98325@gmail.com>
2025-06-15 22:47:59 +0530

    [subhankar] add . _26_Multiplication_table_6_to_9 added

---
+++ control_flow/_26_Multiplication_table_6_to_9.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class _26_Multiplication_table_6_to_9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+
+    }
+}
